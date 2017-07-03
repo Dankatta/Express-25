2d913eff57f5c1a64b8a35615187c3d8dd01c1cf
MonikaTzenova <monikatzenova@gmail.com>
2017-07-03 23:24:58 +0300

    add database and express config files

---
+++ server/config/mongoose.js
@@ -0,0 +1,20 @@
+let mongoose = require('mongoose');
+
+module.exports = function(config) {
+    mongoose.connect(config.db);
+    let db = mongoose.connection;
+
+    db.once('open', function(err) {
+        if (err) {
+            console.log('Database could not be opened: ' + err);
+            return;
+        }
+
+        console.log('Database up and running...')
+    });
+
+    db.on('error', function(err){
+        console.log('Database error: ' + err);
+    });
+
+};
