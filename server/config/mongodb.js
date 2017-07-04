2089dfc2a978b4c298bd8800610b7badd299d943
MonikaTzenova <monikatzenova@gmail.com>
2017-07-04 23:59:12 +0300

    add passport index.js file

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -1,13 +1,7 @@
-let MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
+const { MongoClient } = require('mongodb');
 
-// Connection URL
-let url = 'mongodb://localhost:27017/myproject';
-// Use connect method to connect to the Server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected correctly to server");
+const connect = (connectionString) => {
+    return MongoClient.connect(connectionString);
+};
 
-    db.close();
-});
-
+module.exports = { connect };
