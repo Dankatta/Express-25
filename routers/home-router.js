01e53ee3c598cac48956a0d32ddaaf0300d77c35
Dankatta <dani.dvh@gmail.com>
2017-07-24 20:06:50 +0300

    added basic layout

--- routers/home-router.js
+++ routers/home-router.js
@@ -2,18 +2,11 @@ const express = require('express');
 
 module.exports = function(app, data, models, validator) {
     const controller =
-        require('../controllers/home-controller')(data, models, validator);
+        require('../controller/home-controller')(data, models, validator);
 
     const router = new express.Router();
 
     router
-        .get('/', controller.getHome)
-        .get('/about', controller.getAbout)
-        .get('/personal-doctors', controller.getPersonalDoctorsView)
-        .get('/doctors', controller.getDoctorsView)
-        .get('/doctors-all', controller.getDoctors)
-        .get('/personal-doctors-all', controller.getPersonalDoctors)
-        .get('/reciepes-search', controller.getReciepesSearch);
-
+        .get('/', controller.getHome);
     app.use('/', router);
 };
