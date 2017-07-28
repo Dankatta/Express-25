7ae6f789b1625e2600479d92748851ebe9a992c6
MonikaTzenova <monikatzenova@gmail.com>
2017-07-28 21:06:23 +0300

    fixed login/logout - show user name by login

--- controllers/home-controller.js
+++ controllers/home-controller.js
@@ -1,20 +1,18 @@
 module.exports = function(data, models, validation) {
-    function init(req, result) {
-        function isAuth(req, result){
+    function isAuth(req, result){
         if (req.isAuthenticated()) {
             result.user = req.user.username;
         }
         return result;
     }
-    }
          return {
         getHome(req, res) {
-            const result = init(req, {});
+            const result = isAuth(req, {});
 
             res.render('home/home.pug', { result });
         },
         getEvents(req,res){
-            const result = init(req,{});
+            const result = isAuth(req,{});
             res.render('home/events.pug',{result});
         },
     };
