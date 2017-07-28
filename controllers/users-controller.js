d9e1478da8e35c4e0b408ad117f16dd9529b4a09
MonikaTzenova <monikatzenova@gmail.com>
2017-07-28 21:30:14 +0300

    fix in users section- show users when login

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,18 +1,25 @@
 module.exports = function(data, models, validation) {
-
-         return {
+    function isAuth(req, result){
+        if (req.isAuthenticated()) {
+            result.user = req.user.username;
+        }
+        return result;
+    }
+    return {
              
         getUserbyId(req, res) {
+            const result = isAuth(req, {});
             const id=req.params.id;
             data.findUserById(id)
             .then((user)=>
-            res.render('user/user.pug', { user }));
+            res.render('user/user.pug', { user , result}));
             
         },
         getUsers(req,res) {
+            const result = isAuth(req, {});
             data.getUsers()
             .then((users)=>
-            res.render('user/users.pug',{users}));
+            res.render('user/users.pug',{users, result}));
         },
     };
 };
